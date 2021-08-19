ccdd735477686b0af252ff8ebcd157dd08d033a9
geoffreeve <49611812+geoffreeve@users.noreply.github.com>
2021-08-20 09:48:26 +1200

    initial commit
    support files

---
+++ 00_Website_Template/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Put Content Here">
+    <meta name="keywords" content="Put keywords here">
+    <meta name="author" content="Put your name here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Website Title goes here</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="layout.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        <div class="banner">
+            Banner goes here
+        </div>
+
+        <div class="box nav">
+            Navigation goes here
+        </div>
+
+        <div class = "box main">
+            Main content goes here
+        </div>
+       
+
+        <div class="box footer">
+            CC yourname 20XX
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>        
---
+++ 91903_CC_README.txt
@@ -0,0 +1,9 @@
+*** CC Showcase ***
+
+This folder includes the text and sources for the images used in this project.
+
+The images in the 'insects', 'people' and 'space' folders have been resized.  You can access the original pages via the URLs on the 'supplied_image_sources.txt' file.
+
+You should source at least three images from Wikimedia Commons yourself and resize / optimise them appropriately.  
+
+You have been provided with a *very* basic template which is missing some of the required folders.  It includes the Jquery and fontawesome files (css / fonts).
